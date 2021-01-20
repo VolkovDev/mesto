67172e9fa21b9260f0cec5c67cb42b5b6c2bf002
Andrey Volkov <[fullon1988@gmail.com]>
2021-01-20 16:57:25 +0300

    fix mesto 9

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import './index.css';
 import { Card } from '../components/Card.js'
 import { FormValidator } from '../components/FormValidator.js'
-// import { initialCards } from '../utils/initialCards.js'
 import Section from '../components/Section.js'
 import PopupWithImage from '../components/PopupWithImage.js'
 import UserInfo from '../components/UserInfo.js'
